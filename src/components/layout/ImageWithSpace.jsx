349bd6d906004d9936181e1c0715141694680964
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-08-04 18:00:00 -0300

    Criado componentes e temas

--- src/components/layout/ImageWithSpace.jsx
+++ src/components/layout/ImageWithSpace.jsx
@@ -24,7 +24,7 @@ const StyledImage = styled.div
 
 const StyledContainer = styled.div
 `
-  background-color: white;
+  background-color: ${props => props.theme.white};
   padding: 30px 50px;
 
   @media (min-width: ${WIDTH_BREAK}) {
@@ -33,6 +33,7 @@ const StyledContainer = styled.div
   
   @media (max-width: ${WIDTH_BREAK}) {
     width: 100%;
+    height: 100vh;
   }
 
   display: flex;
