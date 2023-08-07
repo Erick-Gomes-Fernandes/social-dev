05e1977c852c77c8a9701b3a95181854f6f99385
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-08-07 12:20:47 -0300

    Criado navbar

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import Link from 'next/link'
+import Link from "next/link"
 
 import ImageWithSpace from "../src/components/layout/ImageWithSpace"
 import H1 from "../src/components/typography/H1"
